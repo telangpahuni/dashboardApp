9d8fb94a876e3c25d938c793d451d75ed81e330d
Pahuni Telang <telangpahuni@gmail.com>
2021-06-14 20:17:41 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import BarChart from './components/BarChart';
 function App(){
 
   return(
+    <div>
     <div className='container'>
         <BarChart/>
     </div>
+    <h1>dvjskjfhkjs</h1>
+  </div>
   )
 }
 
